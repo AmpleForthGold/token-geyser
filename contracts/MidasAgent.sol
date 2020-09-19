8dc1aacb5ce36ffcd3077c744fb5f9dfc2dc2ef1
Arun <arun@ampleforth.gold>
2020-09-19 21:33:13 +1000

    iminor comment changes.

--- contracts/MidasAgent.sol
+++ contracts/MidasAgent.sol
@@ -10,20 +10,16 @@ import "./TokenPool.sol";
 /**
  * @title MidasAgent
  * @dev A smart-contract based mechanism to distribute tokens over time, inspired loosely by
- *      Compound and Uniswap. Code (mostly) comes from Ampleforth TokenGeyser.Sol.
+ *      Compound and Uniswap. Code (mostly) comes from Ampleforth TokenGeyser-sol.
  *
- *      Distribution tokens are added to a locked pool in the contract and become unlocked over time
- *      according to a once-configurable unlock schedule. Once unlocked, they are available to be
+ *      Distribution tokens are added to the contract (pool) and become available to be 
  *      claimed by users.
  *
- *      A user may deposit tokens to accrue ownership share over the unlocked pool. This owner share
+ *      A user may deposit tokens to accrue ownership share over the distribution pool. This owner share
  *      is a function of the number of tokens deposited as well as the length of time deposited.
- *      Specifically, a user's share of the currently-unlocked pool equals their "deposit-seconds"
+ *      Specifically, a user's share of the distribution pool equals their "deposit-seconds"
  *      divided by the global "deposit-seconds". This aligns the new token distribution with long
  *      term supporters of the project, addressing one of the major drawbacks of simple airdrops.
- *
- *      More background and motivation available at:
- *      https://github.com/ampleforth/RFCs/blob/master/RFCs/rfc-1.md
  */
 contract MidasAgent is IStaking, Ownable {
     using SafeMath for uint256;
